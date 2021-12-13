94aaba25f0a307d8123d853a3707891da5c5dbf8
Tony Bowden <tony@tmtm.com>
2021-12-13 12:27:44 +0200

    initial scraper

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -7,17 +7,17 @@ require 'pry'
 class MemberList
   class Member
     def name
-      noko.css('.name').text.tidy
+      noko.css('.section-item__title').text.tidy
     end
 
     def position
-      noko.css('.position').text.tidy
+      noko.css('.section-item-member__position').text.split(/ [-—] /).map(&:tidy)
     end
   end
 
   class Members
     def member_container
-      noko.css('.member')
+      noko.css('.section-item-member')
     end
   end
 end
